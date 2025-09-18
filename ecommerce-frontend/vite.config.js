2e0e4d6eaaf6c3517a1d74e94f79f67bfdc6a2b9
Mahek-ds-22 <mahek6845@gmail.com>
2025-09-18 20:16:46 +0530

    Create vite.config.js

---
+++ ecommerce-frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/ecommerce-frontend/", // 🔥 important: match your repo name
+});
